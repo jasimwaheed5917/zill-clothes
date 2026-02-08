16a6238a85a629f64551c5fc959df6a75ab37ca2
jasimwaheed5917 <softwaredeveloper5917@gmail.com>
2026-02-08 17:37:24 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-4TPHY9M94Q');
+
+
+
